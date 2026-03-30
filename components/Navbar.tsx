aeefcbdd53d81e5273922591ee99e6ee2e8fd4c1
Holly Clouthier <dealdesk365@gmail.com>
2026-03-30 11:17:30 -0400

    Navbar: push call/text buttons back to the right

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         </div>
 
         {/* Phone CTAs */}
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ml-auto">
           <a
             href="tel:9894709128"
             style={{ backgroundColor: "var(--orange)" }}
